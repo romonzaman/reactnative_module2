bfac6df0b3e0bf72d71836d21cb949f9a8bf6350
mohammad kamruzzaman <romon.zaman@gmail.com>
2025-08-10 08:31:20 +0600

    data file added

---
+++ data.js
@@ -0,0 +1,9 @@
+// data.js
+// Export an array of students. Each student has id, name and scores (3 test scores).
+
+export const students = [
+  { id: 24, name: 'Lina', scores: [65, 70, 75] },
+  { id: 31, name: 'Alex', scores: [85, 90, 80] },
+  { id: 45, name: 'Maya', scores: [95, 88, 92] },
+  { id: 52, name: 'Sam', scores: [70, 65, 75] }
+];
